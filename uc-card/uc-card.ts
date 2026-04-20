d8bc0ccecac893e746e22099af9b909052183ecd
Dživo Jelić <dzivo.jelic@enum.hr>
2026-04-20 17:57:14 +0200

    feat: implement survey question components for various question types (short text, long text, single choice, multiple choice, rating) and update styling for consistency

--- uc-card/uc-card.ts
+++ uc-card/uc-card.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'uc-card',
   imports: [],
   templateUrl: './uc-card.html',
   styleUrl: './uc-card.scss',
+  host: {
+    '[class.uc-card--fill]': 'fit() === "fill"',
+  },
 })
-export class UcCard {}
+export class UcCard {
+  fit = input<'fit' | 'fill'>('fit');
+}
