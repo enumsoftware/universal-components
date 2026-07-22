fbb9699da58a78680460b8cdcc4a7eff15787b7b
Dživo Jelić <dzivo.jelic@uniqcast.com>
2026-07-22 12:57:36 +0200

    feat: enhance accordion component with improved styling and accessibility features

--- uc-accordion/uc-accordion-item.ts
+++ uc-accordion/uc-accordion-item.ts
@@ -18,11 +18,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './uc-accordion-item.css',
 })
 export class UcAccordionItem {
+  private static nextId = 0;
+
   title = input<string>('');
   isOpen = signal<boolean>(false);
   headerTemplate = contentChild('header', { read: TemplateRef });
   contentTemplate = contentChild('content', { read: TemplateRef });
   itemToggled = output<boolean>();
+  readonly panelId = `uc-accordion-panel-${UcAccordionItem.nextId}`;
+  readonly triggerId = `uc-accordion-trigger-${UcAccordionItem.nextId++}`;
 
   toggleOpen() {
     this.isOpen.update((v) => !v);
